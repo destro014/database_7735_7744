424c0223f24c3fd26eb9a5eb1d44126d2c237331
Destro <destro014@users.noreply.github.com>
2017-08-12 13:01:12 +0545

    Landing page
    Started to design the landing page

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="shortcut icon" href="logo.png" type="image/x-icon">
+</head>
+<body>
+<div class ="container">
+	<div id="row" >
+		<div class="image-small">
+			<img src="logo.png" >
+			<form action="#" method="">
+				<ul>
+					<li>
+						<input type="text" name="username" required autocomplete="off">
+						<label for="username">Username</label>
+					</li>
+					<li>
+						<input type="password" name="password" required autocomplete="off">
+						<label for="password">Password</label>
+					</li>
+					<li>
+						<button type="submit" value="submit">Submit</button>
+					</li>
+				</ul>
+			</form>
+		</div>
+	</div>
+</div>
+</body>
+</html>
