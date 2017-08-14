e1b7732cd0fe21b7939352c66654aa3bab2135aa
Nirajnepal <Nirajnepal101@gmail.com>
2017-08-14 16:11:34 +0545

    mistake dherai huna sakxa tei vayera naya file banako

---
+++ database/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+	<title>Blood Quest - Every drop counts</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+</head>
+<body>
+<div class="container">
+	<div id="row">
+		<div class="col-md-12 header">
+			<div class=" col-xs-6 col-sm-6 col-md-6 headergrid">
+					<div class="image-small">
+						<img src="img/logo.png" alt="logo">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-6 loginformgrid">
+					<form action="#" method="" class="login-form">
+						
+						
+							<input type="text" name="username" required autocomplete="off" placeholder="username">
+							<!-- <label for="username"></label> -->
+							<input type="password" name="password" required autocomplete="off" placeholder="password">
+							<!-- /<label for="password">Password</label> -->
+							<button class="login-button" type="submit" value="submit">Login</button>
+						
+						<div class="belowloginformgrid">
+							<div class="rememberme">
+								<input type="checkbox" name="remeberme"> <span class="asd">Remember me</span>
+							</div>
+							<div class="forgetpassword">
+								<a href="#">Forgot password?</a>
+							</div>
+						</div>
+						
+					</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+   <div class="row">
+	      <div class="belowheadergrid">
+			<div class="col-md-7">
+				<div class="bloodtype">
+					<h1 class="bloodavailabletitle">Available blood for you.</h1>
+					<div class="bloodtypegrid">
+						<div class="image-bloodtype">
+							<img src="img/bloodtype/A-.png" alt="A-">
+						</div>
+						<div class="image-bloodtype">
+							<img src="img/bloodtype/A+.png" alt="A+">
+						</div>
+						<div class="image-bloodtype">
+							<img src="img/bloodtype/AB-.png" alt="AB-">
+						</div>
+						<div class="image-bloodtype">
+							<img src="img/bloodtype/AB+.png" alt="AB+">
+						</div>
+						<div class="image-bloodtype">
+							<img src="img/bloodtype/B-.png" alt="B-">
+						</div>
+						<div class="image-bloodtype">
+							<img src="img/bloodtype/B+.png" alt="B+">
+						</div>
+						<div class="image-bloodtype">
+							<img src="img/bloodtype/O-.png" alt="O-">
+						</div>
+						<div class="image-bloodtype">
+							<img src="img/bloodtype/O+.png" alt="O+">
+						</div>
+					</div>
+				</div>
+			</div>
+		<div class="col-md-5">
+			<div class="registerform">
+				<h1 class="registertitle">Register now</h1>
+				<form>
+					<input type="text" name="firstname" required autocomplete="off">
+					<label for="firstname">Firstname</label>
+
+					<input type="text" name="lastname" required autocomplete="off">
+					<label for="lastname">Lastname</label>
+					<br><br>
+					<input type="email" name="email" required autocomplete="off">
+					<label for="email">Email</label><br>
+					<br><button class="login-button-register" type="submit" value="submit">Continue</button>
+				</form>
+			</div>
+		</div>
+	</div>
+  </div>
+</div>
+       <script src="js/jquery-3.2.1.js" type="text/javascript"></script>
+       <script src="js/bootstrap.min.js" type="text/javascript"></script>
+</body>
+</html>
+<!--
+<div class="bloodavailable">
+	<h1 class="bloodavailabletitle">Available blood for you.</h1>
+	<div class="bloodtype">
+		<div class="image-bloodtype">
+				<img src="img/bloodtype/A-.png" >
+		</div>
+		<div class="image-bloodtype">
+				<img src="img/bloodtype/A+.png" >
+		</div>
+		<div class="image-bloodtype">
+				<img src="img/bloodtype/AB-.png" >
+		</div>
+		<div class="image-bloodtype">
+				<img src="img/bloodtype/AB+.png" >
+		</div>
+		<div class="divider"></div>
+		<div class="image-bloodtype">
+				<img src="img/bloodtype/B-.png" >
+		</div>
+		<div class="image-bloodtype">
+				<img src="img/bloodtype/B+.png" >
+		</div>
+		<div class="image-bloodtype">
+				<img src="img/bloodtype/O-.png" >
+		</div>
+		<div class="image-bloodtype">
+				<img src="img/bloodtype/O+.png" >
+		</div>
+	</div>
+</div> -->
