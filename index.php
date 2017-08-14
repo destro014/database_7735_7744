67f27142d8424491ff8413bf93e691bbb066f7b8
Destro <japmc11@gmail.com>
2017-08-14 12:16:21 +0545

    Slight change in style.

--- index.php
+++ index.php
@@ -3,43 +3,88 @@
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <head>
-	<title></title>
+	<title>Blood Quest - Every drop counts</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
 </head>
 <body>
-<div class ="container">
-	<div id="row" >
-		<div class="image-small">
-			<img src="img/logo.png" >
+<div id="row">
+	<div class="headergrid">
+		<div>
+			<div class="image-small">
+				<img src="img/logo.png" alt="logo">
+			</div>
 		</div>
-			<form action="#" method="">
-				
-						<input type="text" name="username" required autocomplete="off">
-						<label for="username">Username</label>
-						<input type="password" name="password" required autocomplete="off">
-						<label for="password">Password</label>
-						<button class="login-button" type="submit" value="submit">Submit</button>
-						<div class="left-padding"></div>
-				
+		<div>
+			<form action="#" method="" class="login-form">
+				<div class="loginformgrid">
+				<div>
+					<input type="text" name="username" required autocomplete="off">
+					<label for="username">Username</label>
+					<input type="password" name="password" required autocomplete="off">
+					<label for="password">Password</label>
+					<button class="login-button" type="submit" value="submit">Login</button>
+				</div>
+				<div class="belowloginformgrid">
+					<div>
+						<input type="checkbox" name="remeberme"> Remember me
+					</div>
+					<div class="forgetpassword">
+						<a href="#">Forgot password?</a>
+					</div>
+				</div>
+				</div>
 			</form>
+		</div>
 	</div>
 </div>
-<!-- <div class="container">
-<h1 class="registertext">Register now</h1>
-	<form>
-		<input type="text" name="firstname" required autocomplete="off">
-		<label for="firstname">firstname</label>
-		<input type="text" name="lastname" required autocomplete="off">
-		<label for="lastname">lastname</label>
-		<br>
-		<input type="emailemail" name="email" required autocomplete="off">
-		<label for="email">email</label>
-		
-		<br><button class="login-button" type="submit" value="submit">Submit</button>
-		
-	</form>
-</div> -->
+<div class="belowheadergrid">
+	<div class="bloodtype">
+		<h1 class="bloodavailabletitle">Available blood for you.</h1>
+		<div class="bloodtypegrid">
+			<div class="image-bloodtype">
+				<img src="img/bloodtype/A-.png" alt="A-">
+			</div>
+			<div class="image-bloodtype">
+				<img src="img/bloodtype/A+.png" alt="A+">
+			</div>
+			<div class="image-bloodtype">
+				<img src="img/bloodtype/AB-.png" alt="AB-">
+			</div>
+			<div class="image-bloodtype">
+				<img src="img/bloodtype/AB+.png" alt="AB+">
+			</div>
+			<div class="image-bloodtype">
+				<img src="img/bloodtype/B-.png" alt="B-">
+			</div>
+			<div class="image-bloodtype">
+				<img src="img/bloodtype/B+.png" alt="B+">
+			</div>
+			<div class="image-bloodtype">
+				<img src="img/bloodtype/O-.png" alt="O-">
+			</div>
+			<div class="image-bloodtype">
+				<img src="img/bloodtype/O+.png" alt="O+">
+			</div>
+		</div>
+	</div>
+	<div class="registerform">
+		<h1 class="registertitle">Register now</h1>
+		<form>
+			<input type="text" name="firstname" required autocomplete="off">
+			<label for="firstname">firstname</label>
+			<input type="text" name="lastname" required autocomplete="off">
+			<label for="lastname">lastname</label>
+			<br>
+			<input type="email" name="email" required autocomplete="off">
+			<label for="email">email</label>
+			<br><button class="login-button" type="submit" value="submit">Continue</button>
+		</form>
+	</div>
+</div>
+</body>
+</html>
+<!--
 <div class="bloodavailable">
 	<h1 class="bloodavailabletitle">Available blood for you.</h1>
 	<div class="bloodtype">
@@ -69,6 +114,4 @@
 				<img src="img/bloodtype/O+.png" >
 		</div>
 	</div>
-</div>
-</body>
-</html>
+</div> -->
