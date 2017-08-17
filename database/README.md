66dcd082ed2cb8c7ae4fd3af168e296c22ec369c
Destro <japmc11@gmail.com>
2017-08-17 17:37:10 +0545

    New Changes

--- database/README.md
+++
@@ -1,2 +0,0 @@
-# database_7735_7744
-Repository to maintain and keep our code of project.
--- index.php
+++ index.php
@@ -4,114 +4,107 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <head>
 	<title>Blood Quest - Every drop counts</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+	<!-- jQuery library -->
+	<script src="js/jquery-3.2.1.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="js/bootstrap.min.js"></script>
+
 </head>
 <body>
-<div id="row">
-	<div class="headergrid">
-		<div>
-			<div class="image-small">
-				<img src="img/logo.png" alt="logo">
-			</div>
+<nav class="navbar-landing navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#"><img src="img/logo.png" alt="logo" class="image-logo"></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+      <div class="loginform">
+    	<form action="#" method="" class="login-form">
+				<li>
+				<input type="text" name="username" required  autocomplete="off">
+				<label for="username">Username</label>
+				<input type="password" name="password" required autocomplete="off" >
+				<label for="password">Password</label>
+				<button class="login-button" type="submit" value="submit">Login</button>
+				</li>
+				<li>
+				<input type="checkbox" name="remeberme"> Remember me
+
+				<a href="#" class="forgetpassword">Forgot password?</a>
+
+				</li>
+		</form>
 		</div>
-		<div>
-			<form action="#" method="" class="login-form">
-				<div class="loginformgrid">
-				<div>
-					<input type="text" name="username" required autocomplete="off">
-					<label for="username">Username</label>
-					<input type="password" name="password" required autocomplete="off">
-					<label for="password">Password</label>
-					<button class="login-button" type="submit" value="submit">Login</button>
-				</div>
-				<div class="belowloginformgrid">
-					<div>
-						<input type="checkbox" name="remeberme"> Remember me
-					</div>
-					<div class="forgetpassword">
-						<a href="#">Forgot password?</a>
-					</div>
-				</div>
-				</div>
-			</form>
-		</div>
-	</div>
-</div>
-<div class="belowheadergrid">
+		</li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<div class="col-md-7">
 	<div class="bloodtype">
 		<h1 class="bloodavailabletitle">Available blood for you.</h1>
-		<div class="bloodtypegrid">
-			<div class="image-bloodtype">
-				<img src="img/bloodtype/A-.png" alt="A-">
-			</div>
-			<div class="image-bloodtype">
-				<img src="img/bloodtype/A+.png" alt="A+">
-			</div>
-			<div class="image-bloodtype">
-				<img src="img/bloodtype/AB-.png" alt="AB-">
-			</div>
-			<div class="image-bloodtype">
-				<img src="img/bloodtype/AB+.png" alt="AB+">
-			</div>
-			<div class="image-bloodtype">
-				<img src="img/bloodtype/B-.png" alt="B-">
-			</div>
-			<div class="image-bloodtype">
-				<img src="img/bloodtype/B+.png" alt="B+">
-			</div>
-			<div class="image-bloodtype">
-				<img src="img/bloodtype/O-.png" alt="O-">
-			</div>
-			<div class="image-bloodtype">
-				<img src="img/bloodtype/O+.png" alt="O+">
+		<div class="image-bloodtype col-md-3">
+			<img src="img/bloodtype/A-.png" alt="A-">
+		</div>
+		<div class="image-bloodtype col-md-3">
+			<img src="img/bloodtype/A+.png" alt="A+">
+		</div>
+		<div class="image-bloodtype col-md-3">
+			<img src="img/bloodtype/B-.png" alt="B-">
+		</div>
+		<div class="image-bloodtype col-md-3">
+			<img src="img/bloodtype/B+.png" alt="B+">
+		</div>	
+		<div class="image-bloodtype col-md-3">
+			<img src="img/bloodtype/AB-.png" alt="AB-">
+		</div>
+		<div class="image-bloodtype col-md-3">
+			<img src="img/bloodtype/AB+.png" alt="AB+">
+		</div>
+		<div class="image-bloodtype col-md-3">
+			<img src="img/bloodtype/O-.png" alt="O-">
+		</div>
+		<div class="image-bloodtype col-md-3">
+			<img src="img/bloodtype/O+.png" alt="O+">
+		</div>
+	</div>	
+	<div class="buttoncenter">
+	<a href="details.php"><button class="details">Details</button></a>
+	</div>						
+</div>
+	<div class="col-md-5 ">
+		<div class="registerform">
+			<h1 class="registernowtitle">Register now</h1>
+			<form class="registerform">
+				<input type="text" name="firstname" required autocomplete="off">
+				<label for="firstname">Firstname</label>
+				<input type="text" name="lastname" required autocomplete="off">
+				<label for="lastname">Lastname</label>
+				<br><br>
+				<input type="email" name="email" required autocomplete="off">
+				<label for="email">Email</label><br>
+				<br>
+				<div class="buttoncenter">
+					<button class="login-button-register" type="submit" value="submit">Continue</button>
+				</div>
+				</form>
 			</div>
 		</div>
 	</div>
-	<div class="registerform">
-		<h1 class="registertitle">Register now</h1>
-		<form>
-			<input type="text" name="firstname" required autocomplete="off">
-			<label for="firstname">firstname</label>
-			<input type="text" name="lastname" required autocomplete="off">
-			<label for="lastname">lastname</label>
-			<br>
-			<input type="email" name="email" required autocomplete="off">
-			<label for="email">email</label>
-			<br><button class="login-button" type="submit" value="submit">Continue</button>
-		</form>
-	</div>
+  </div>
 </div>
 </body>
 </html>
-<!--
-<div class="bloodavailable">
-	<h1 class="bloodavailabletitle">Available blood for you.</h1>
-	<div class="bloodtype">
-		<div class="image-bloodtype">
-				<img src="img/bloodtype/A-.png" >
-		</div>
-		<div class="image-bloodtype">
-				<img src="img/bloodtype/A+.png" >
-		</div>
-		<div class="image-bloodtype">
-				<img src="img/bloodtype/AB-.png" >
-		</div>
-		<div class="image-bloodtype">
-				<img src="img/bloodtype/AB+.png" >
-		</div>
-		<div class="divider"></div>
-		<div class="image-bloodtype">
-				<img src="img/bloodtype/B-.png" >
-		</div>
-		<div class="image-bloodtype">
-				<img src="img/bloodtype/B+.png" >
-		</div>
-		<div class="image-bloodtype">
-				<img src="img/bloodtype/O-.png" >
-		</div>
-		<div class="image-bloodtype">
-				<img src="img/bloodtype/O+.png" >
-		</div>
-	</div>
-</div> -->
